7976879d71b16493dad795dd239f4802daccd822
Diego ROJAS <rojasdiegopro@gmail.com>
2023-09-09 19:58:56 +0800

    Fix mamba installation script

--- roles/computer/files/install-mambaforge.sh
+++ roles/computer/files/install-mambaforge.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
-macos-ansible git:(main) ✗ export MAMBAFORGE_DIST=Mambaforge-$(uname)-$(uname -m).sh
+
+set -e
+
+export MAMBAFORGE_DIST=Mambaforge-$(uname)-$(uname -m).sh
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/$MAMBAFORGE_DIST"
 bash $MAMBAFORGE_DIST -b -p ~/mambaforge && rm $MAMBAFORGE_DIST
 ~/mambaforge/bin/conda init zsh
